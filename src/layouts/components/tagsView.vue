05ef80fa43739a3491b600ffa5667aaa59ac8889
KYUUTA <200993697@qq.com>
2020-09-09 00:46:30 +0800

    推动tagsview进度

--- src/layouts/components/tagsView.vue
+++ src/layouts/components/tagsView.vue
@@ -1,37 +1,50 @@
 <template>
-    <div class="tabs-main">
-        <a-button
-            icon="left"
-            size="large"
-            @click="handleScroll(1)"/>
+    <div class="tags-main">
+        <a-icon
+            v-show="!showMoveBtn"
+            class="move-btn"
+            type="left"
+            @click="handleScroll(1)" />
         <div
             ref="scrollOuter"
-            class="test-wrapper"
+            class="tag-wrapper"
             @DOMMouseScroll="DOMhandleScroll"
             @mousewheel="DOMhandleScroll">
             <div
                 ref="scrollBody"
-                class="test-content"
+                class="tag-content"
                 :style="{
                     'transform': `translateX(-${tagBodyLeft}px)`
                 }">
                 <div
-                    class="tag"
+                    :class="[
+                        'tag',
+                        {
+                            'tag-active': tag.name === $route.name
+                        }
+                    ]"
                     v-for="tag in visitedViews"
                     :key="tag.name"
-                    @click="handleClickTag($event)">{{ $t(tag.meta.title) }}</div>
+                    :ref="tag.name"
+                    @click="handleClickTag(tag.name)">
+                    <span>{{ $t(tag.meta.title) }}</span>
+                    <div class="close-btn" @click.stop="delTag(tag)">
+                        <a-icon type="close" />
+                    </div>
+                </div>
             </div>
         </div>
-        <a-button
-            icon="right"
-            size="large"
-            @click="handleScroll(-1)"/>
-
+        <a-icon
+            v-show="!showMoveBtn"
+            class="move-btn"
+            type="right"
+            @click="handleScroll(-1)" />
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import { throttle } from '@/utils'
 
 export default {
     name: 'TagsView',
@@ -39,7 +52,8 @@ export default {
         return {
             tagBodyLeft: 0,
             outerWidth: 0,
-            bodyWidth: 0
+            bodyWidth: 0,
+            showMoveBtn: false
         }
     },
     computed: {
@@ -50,6 +64,7 @@ export default {
     watch: {
         $route() {
             this.addTags()
+            this.initElWidth()
         },
         outerWidth(newval, oldval) {
             if ((oldval + this.tagBodyLeft) === this.bodyWidth) {
@@ -59,7 +74,6 @@ export default {
     },
     mounted() {
         this.initElWidth()
-        console.log(this.visitedViews)
         window.addEventListener('resize', () => {
             this.initElWidth()
             if (this.bodyWidth < this.outerWidth) {
@@ -68,9 +82,15 @@ export default {
         })
     },
     methods: {
-        initElWidth() {
-            this.outerWidth = this.$refs.scrollOuter.offsetWidth
-            this.bodyWidth = this.$refs.scrollBody.offsetWidth
+        initElWidth: throttle(function () {
+            this.setMoveBtnState()
+            this.$nextTick(() => {
+                this.outerWidth = this.$refs.scrollOuter.offsetWidth
+                this.bodyWidth = this.$refs.scrollBody.offsetWidth
+            })
+        }, 56),
+        setMoveBtnState() {
+            this.showMoveBtn = this.bodyWidth <= this.outerWidth
         },
         DOMhandleScroll(e) {
             e.preventDefault()
@@ -81,11 +101,11 @@ export default {
             }
             this.handleScroll(delta)
         },
-        handleScroll(offset) {
+        handleScroll(offset, movingDistance = this.bodyWidth) {
             if (offset > 0) {
-                this.tagBodyLeft = Math.max(0, this.tagBodyLeft - 240)
+                this.tagBodyLeft = Math.max(0, this.tagBodyLeft - movingDistance)
             } else {
-                const translateX = this.tagBodyLeft + 240
+                const translateX = this.tagBodyLeft + movingDistance
                 if (this.outerWidth < this.bodyWidth) {
                     if (translateX > this.bodyWidth - this.outerWidth) {
                         this.tagBodyLeft = this.bodyWidth - this.outerWidth
@@ -102,26 +122,60 @@ export default {
 
             if (name) {
                 this.$store.dispatch('AddView', this.$route)
-
-                this.$nextTick(() => {
-                    this.bodyWidth = this.$refs.scrollBody.offsetWidth
-                    this.tagBodyLeft = this.bodyWidth - this.outerWidth
-                })
+                    .then(exists => {
+                        if (exists) {
+                            this.setMoveBtnState()
+                            this.$nextTick(() => {
+                                this.moveTag(name)
+                            })
+                        } else {
+                            this.$nextTick(() => {
+                                this.bodyWidth = this.$refs.scrollBody.offsetWidth
+                                this.tagBodyLeft = this.bodyWidth - this.outerWidth
+                            })
+                        }
+                    })
             }
 
             return false
         },
-        handleClickTag(e) {
-            const targetOffsetLeft = e.target.offsetLeft + e.target.offsetWidth
+        delTag(tag) {
+            this.$store.dispatch('DeleteView', tag.name)
+                .then(visitedViews => {
+                    if (tag.path === this.$route.path) {
+                        const latestView = visitedViews.slice(-1)[0]
+                        if (latestView) {
+                            this.$router.push(latestView.fullPath)
+                        } else {
+                            this.$router.push('/dashboard')
+                        }
+                    }
+                })
+        },
+        handleClickTag(name) {
+            this.moveTag(name)
+
+            if (name !== this.$route.name) {
+                this.$router.push({
+                    name: name
+                })
+            }
+        },
+        moveTag(name) {
+            const elOffsetLeft = this.$refs[name][0].offsetLeft
+            const elOffsetWidth = this.$refs[name][0].offsetWidth
+            const targetOffsetLeft = elOffsetLeft + elOffsetWidth
             const passedArea = this.tagBodyLeft + this.outerWidth
 
-            if ((e.target.offsetLeft > this.tagBodyLeft) &&
+            // 标签在可视区域
+            if ((elOffsetLeft > this.tagBodyLeft) &&
                 (targetOffsetLeft < passedArea)) {
                 return false
             }
 
-            if (this.tagBodyLeft > e.target.offsetLeft) {
-                this.tagBodyLeft = e.target.offsetLeft
+            // 标签移动到可视范围内最后一个或最前一个
+            if (this.tagBodyLeft > elOffsetLeft) {
+                this.tagBodyLeft = elOffsetLeft
             } else if (targetOffsetLeft > passedArea) {
                 this.tagBodyLeft = Math.abs(passedArea - targetOffsetLeft) + this.tagBodyLeft
             }
@@ -131,29 +185,66 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    .tabs-main {
-        position: relative;
-        background-color: #f5f7f9;
-        padding: 6px 10px;
+    .tags-main {
+        position: fixed;
+        top: 64px;
+        padding: 6px 8px;
         display: flex;
-        .test-wrapper {
-            background-color: skyblue;
+        z-index: 8;
+        width: 100%;
+        background-color: #f0f2f5;
+        .move-btn {
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 14px;
+            padding: 0 3px;
+            color: #515a6e;
+        }
+        .tag-wrapper {
             flex: 1;
             height: 100%;
-            margin: 0 10px;
             overflow: hidden;
             white-space: nowrap;
-            .test-content {
+            .tag-content {
                 height: 100%;
                 float: left;
                 display: flex;
+                align-items: center;
                 transition: transform .5s ease-in-out;
                 .tag {
-                    border: 1px solid red;
-                    height: 100%;
-                    padding: 0 10px;
-                    margin-right: 5px;
-                    line-height: 40px;
+                    cursor: pointer;
+                    display: flex;
+                    align-items: center;
+                    height: 32px;
+                    border-radius: 3px;
+                    margin-right: 6px;
+                    padding: 5px 14px;
+                    background-color: #fff;
+                    color: #808695;
+                    box-sizing: border-box;
+                    transition: color .3s ease-in-out;
+                    &:hover {
+                        color: #515a6e;
+                    }
+                    &-active {
+                        color: #2d8cf0 !important;
+                    }
+                    span {
+                        margin-right: 3px;
+
+                    }
+                    .close-btn {
+                        height: 20px;
+                        line-height: 20px;
+                        font-size: 14px;
+                        color: #999;
+                        transition: color .3s ease-in-out;
+                        &:hover {
+                            color: #515a6e;
+                        }
+                    }
                     &:last-child {
                         margin-right: 0;
                     }
